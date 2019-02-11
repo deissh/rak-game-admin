76ba23ed9318ee5b79040f0f9caf67329a000d91
deissh <kudryavtsev@nppsatek.ru>
2019-02-11 21:28:39 +0300

    add default values

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,2 +1,8 @@
 class Quest < ApplicationRecord
+    before_save :default_values
+    def default_values
+      self.sequence ||= 0
+      self.npc_id ||= 0
+      self.body ||= ''
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    before_save :default_values
+    def default_values
+      self.points ||= 0
+      self.quest_count ||= 0
+    end
 end
